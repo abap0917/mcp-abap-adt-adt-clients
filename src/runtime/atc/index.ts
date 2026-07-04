579751a79738699d25a16684066438a259f5a6a5
abap0917 <abap0917@163.com>
2026-07-04 16:00:31 +0800

    Add src/runtime/atc/index.ts

---
+++ src/runtime/atc/index.ts
@@ -0,0 +1,10 @@
+/**
+ * ATC (ABAP Test Cockpit) - Exports
+ */
+
+export { AtcLog } from './AtcLog';
+export {
+  getCheckFailureLogs,
+  getExecutionLog,
+  type IGetCheckFailureLogsOptions,
+} from './logs';
