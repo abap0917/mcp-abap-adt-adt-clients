138a363e8ea118697adf9eda31f99bb7673f6d62
abap0917 <abap0917@163.com>
2026-07-04 15:57:03 +0800

    Add src/core/interface/delete.ts

---
+++ src/core/interface/delete.ts
@@ -0,0 +1,112 @@
+/**
+ * Interface delete operations - Low-level functions
+ */
+
+import type {
+  IAdtResponse as AxiosResponse,
+  IAbapConnection,
+} from '@mcp-abap-adt/interfaces';
+import {
+  ACCEPT_DELETION,
+  ACCEPT_DELETION_CHECK,
+  CT_DELETION,
+  CT_DELETION_CHECK,
+} from '../../constants/contentTypes';
+import { encodeSapObjectName } from '../../utils/internalUtils';
+import { getTimeout } from '../../utils/timeouts';
+import type { IDeleteInterfaceParams } from './types';
+
+/**
+ * Low-level: Check if interface can be deleted
+ */
+export async function checkDeletion(
+  connection: IAbapConnection,
+  params: IDeleteInterfaceParams,
+): Promise<AxiosResponse> {
+  const { interface_name } = params;
+
+  if (!interface_name) {
+    throw new Error('interface_name is required');
+  }
+
+  const encodedName = encodeSapObjectName(interface_name);
+  const objectUri = `/sap/bc/adt/oo/interfaces/${encodedName}`;
+
+  const checkUrl = `/sap/bc/adt/deletion/check`;
+
+  const xmlPayload = `<?xml version="1.0" encoding="UTF-8"?>
+<del:checkRequest xmlns:del="http://www.sap.com/adt/deletion" xmlns:adtcore="http://www.sap.com/adt/core">
+  <del:object adtcore:uri="${objectUri}"/>
+</del:checkRequest>`;
+
+  const headers = {
+    Accept: ACCEPT_DELETION_CHECK,
+    'Content-Type': CT_DELETION_CHECK,
+  };
+
+  return await connection.makeAdtRequest({
+    url: checkUrl,
+    method: 'POST',
+    timeout: getTimeout('default'),
+    data: xmlPayload,
+    headers,
+  });
+}
+
+/**
+ * Low-level: Delete interface
+ */
+export async function deleteInterface(
+  connection: IAbapConnection,
+  params: IDeleteInterfaceParams,
+): Promise<AxiosResponse> {
+  const { interface_name, transport_request } = params;
+
+  if (!interface_name) {
+    throw new Error('interface_name is required');
+  }
+
+  const encodedName = encodeSapObjectName(interface_name);
+  const objectUri = `/sap/bc/adt/oo/interfaces/${encodedName}`;
+
+  const deletionUrl = `/sap/bc/adt/deletion/delete`;
+
+  // Interfaces require empty transportNumber tag
+  let transportNumberTag = '';
+  if (transport_request?.trim()) {
+    transportNumberTag = `<del:transportNumber>${transport_request}</del:transportNumber>`;
+  } else {
+    transportNumberTag = '<del:transportNumber/>';
+  }
+
+  const xmlPayload = `<?xml version="1.0" encoding="UTF-8"?>
+<del:deletionRequest xmlns:del="http://www.sap.com/adt/deletion" xmlns:adtcore="http://www.sap.com/adt/core">
+  <del:object adtcore:uri="${objectUri}">
+    ${transportNumberTag}
+  </del:object>
+</del:deletionRequest>`;
+
+  const headers = {
+    Accept: ACCEPT_DELETION,
+    'Content-Type': CT_DELETION,
+  };
+
+  const response = await connection.makeAdtRequest({
+    url: deletionUrl,
+    method: 'POST',
+    timeout: getTimeout('default'),
+    data: xmlPayload,
+    headers,
+  });
+
+  return {
+    ...response,
+    data: {
+      success: true,
+      interface_name,
+      object_uri: objectUri,
+      transport_request: transport_request || 'local',
+      message: `Interface ${interface_name} deleted successfully`,
+    },
+  } as AxiosResponse;
+}
