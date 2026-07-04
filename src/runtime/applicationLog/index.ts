5c809de2088577c87e5f9b9353f886e09d6d48c4
abap0917 <abap0917@163.com>
2026-07-04 16:00:28 +0800

    Add src/runtime/applicationLog/index.ts

---
+++ src/runtime/applicationLog/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Application Log - Exports
+ */
+
+export { ApplicationLog } from './ApplicationLog';
+export {
+  getApplicationLogObject,
+  getApplicationLogSource,
+  type IGetApplicationLogObjectOptions,
+  type IGetApplicationLogSourceOptions,
+  validateApplicationLogName,
+} from './read';
