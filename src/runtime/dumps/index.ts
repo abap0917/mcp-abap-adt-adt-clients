39a66e13c00e6ba3fcd904cc6a816020c49f6863
abap0917 <abap0917@163.com>
2026-07-04 16:00:44 +0800

    Add src/runtime/dumps/index.ts

---
+++ src/runtime/dumps/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Runtime Dumps - Exports
+ */
+
+export { RuntimeDumps } from './RuntimeDumps';
+export {
+  buildDumpIdPrefix,
+  buildRuntimeDumpsUserQuery,
+  getRuntimeDumpById,
+  type IRuntimeDumpReadOptions,
+  type IRuntimeDumpReadView,
+  type IRuntimeDumpsListOptions,
+  listRuntimeDumps,
+  listRuntimeDumpsByUser,
+} from './read';
